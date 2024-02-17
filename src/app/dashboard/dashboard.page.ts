bb68ecb8624e851664b0cbe4897d423833851c69
samir <115088723+samirseraj03@users.noreply.github.com>
2024-02-17 13:37:38 +0100

    bostrap , supabase , dashbord azure

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import * as atlas from 'azure-maps-control'; // Or the correct path if different
 
 
@@ -13,48 +13,69 @@ declare const Microsoft: any; // Declaración para permitir el acceso a la API d
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
 })
-export class DashboardPage implements  OnInit {
+export class DashboardPage  {
 
   title = 'azure-maps-web-sdk-test'
   map : any
+  latitude : any 
+  longitude : any
 
 
 
   constructor() { }
 
-  ngOnInit(): void {
-    this.map = new atlas.Map('myMap', {
-      zoom : 11,
-      authOptions: {
-        authType: atlas.AuthenticationType.subscriptionKey,
-        subscriptionKey: 'qbTL3CFdHbknZkT8RdqkemI1AY3oIM21Uci4G02bLHU'
-      }
-    });
-    
+  
+
+ async ngAfterViewInit() {
+  
+    await this.getGeolocation()
+    this.GetMap()
+    await this.initializeMap()
   }
-
-  ngAfterViewInit() {
-  //  this.loadMap();
-
-    //this.getMap();
-    // this.initializeMap(); // Llama a la función initializeMap() en ngOnInit()
-  }
-
-
-
 
   GetMap() {
 
+    console.log(this.latitude);
+    console.log(this.longitude);
+
     try {
-     this.map = new atlas.Map('map', {
-      center: [-118.270293, 34.039737], // Coordenadas para centrar el mapa (Longitud, Latitud)
-      zoom: 12, // Nivel de zoom inicial
-      view: 'Auto',
+     this.map = new atlas.Map('myMap', {
+      center: new atlas.data.Position(this.longitude, this.latitude),
+      zoom: 20, // Nivel de zoom inicial
+      showLogo : false,
+      showFeedback: false ,
+      showFeedbackLink : false,
+      showTileBoundaries : false,
+      language : 'es-ES' ,
+      showBuildingModels : true,
+      style : 'night',
       authOptions: {
         authType: atlas.AuthenticationType.subscriptionKey,
         subscriptionKey: 'qbTL3CFdHbknZkT8RdqkemI1AY3oIM21Uci4G02bLHU' // También puedes pasar la clave de API aquí
       }
+    
     });
+
+    this.map.controls.add([
+      new atlas.control.ZoomControl(),
+      new atlas.control.CompassControl(),
+      new atlas.control.PitchControl(),
+      new atlas.control.StyleControl()
+    ] , {
+      position : atlas.ControlPosition.TopRight
+    })
+
+    
+    this.map.events.add('ready' , () =>{
+
+      var dataSoruce = new atlas.source.DataSource();
+      this.map.sources.add(dataSoruce);
+
+      var layer  = new atlas.layer.SymbolLayer(dataSoruce);
+      this.map.layers.add(layer)
+      dataSoruce.add(new atlas.data.Point([this.longitude, this.latitude]))
+
+    })
     console.log(this.map);
 
   }
@@ -65,16 +86,20 @@ export class DashboardPage implements  OnInit {
 }
 
 
-  getGeolocation(): Promise<{ latitude: number, longitude: number }> {
+  getGeolocation(): Promise<{ latitude_2: number, longitude_2: number }> {
     return new Promise((resolve, reject) => {
       if (navigator.geolocation) {
+        let latitude_2
+        let longitude_2
         navigator.geolocation.getCurrentPosition(
           position => {
-            const latitude = position.coords.latitude;
-            const longitude = position.coords.longitude;
-            console.log("Latitude: " + latitude);
-            console.log("Longitude: " + longitude);
-            resolve({ latitude, longitude });
+            latitude_2 = position.coords.latitude
+            longitude_2 = position.coords.longitude
+            this.latitude = position.coords.latitude;
+            this.longitude = position.coords.longitude;
+            console.log("Latitude: " + this.latitude);
+            console.log("Longitude: " + this.longitude);
+            resolve({ latitude_2, longitude_2 });
           },
           error => {
             console.log("Error al obtener la ubicación: " + error.message);
@@ -108,25 +133,27 @@ export class DashboardPage implements  OnInit {
 
   initializeMap(): void {
 
-
-
-
     const locations = [
       { "name": "Ubicación 1", "latitude": 40.7128, "longitude": -74.0060 },
       { "name": "Ubicación 2", "latitude": 34.0522, "longitude": -118.2437 },
       { "name": "Ubicación 3", "latitude": 51.5074, "longitude": -0.1278 }
     ];
 
-    this.getGeolocation().then(({ latitude, longitude }) => {
-      const map = new Microsoft.Maps.Map('#map');
-      const currentLocation = new Microsoft.Maps.Location(latitude, longitude);
-      map.setView({ center: currentLocation, zoom: 15 });
-      const currentMarker = new Microsoft.Maps.Pushpin(currentLocation, { title: 'Ubicación Actual' });
-      map.entities.push(currentMarker);
+    this.getGeolocation().then(({ latitude_2, longitude_2 }) => {
+      //const map = new Microsoft.Maps.Map('#mymap');
+      const currentLocation = new atlas.data.Position(longitude_2, latitude_2);
+      this.map.setCamera({ center: currentLocation, zoom: 25 });
+      //const currentMarker = new Microsoft.Maps.Pushpin(currentLocation, { title: 'Ubicación Actual' });
+      //map.entities.push(currentMarker);
       for (const location of locations) {
-        const markerLocation = new Microsoft.Maps.Location(location.latitude, location.longitude);
-        const marker = new Microsoft.Maps.Pushpin(markerLocation, { title: location.name });
-        map.entities.push(marker);
+        this.map.events.add('ready' , () =>{
+          var dataSoruce = new atlas.source.DataSource();
+          this.map.sources.add(dataSoruce);
+          var layer  = new atlas.layer.SymbolLayer(dataSoruce);
+          this.map.layers.add(layer)
+          dataSoruce.add(new atlas.data.Point([location.longitude, location.latitude  ]))
+    
+        })
       }
     }).catch(error => {
       console.error("Error:", error);
