90a5c5233fb42bbdc34cec58615aef8b6678ad2e
Isam <nfs.issamhajji@gmail.com>
2024-02-27 20:03:03 +0100

    Merging changes from local to dev branch
    Added Login and register pages.
    Added supabase authentication

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,78 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { isPlatform } from '@ionic/angular';
+import { createClient, SupabaseClient, User } from '@supabase/supabase-js';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private supabase: SupabaseClient
+  private currentUser: BehaviorSubject<User | boolean> = new BehaviorSubject<User | boolean>(false)
+
+  constructor(private router: Router) {
+    this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey)
+
+    this.supabase.auth.onAuthStateChange((event, sess) => {
+      if (event === 'SIGNED_IN' || event === 'TOKEN_REFRESHED') {
+        console.log('SET USER')
+
+        this.currentUser.next(sess!.user)
+      } else {
+        this.currentUser.next(false)
+      }
+    })
+
+    // Trigger initial session load
+    this.loadUser()
+  }
+
+  async loadUser() {
+    if (this.currentUser.value) {
+      // User is already set, no need to do anything else
+      return
+    }
+    const user = await this.supabase.auth.getUser()
+
+    if (user.data.user) {
+      this.currentUser.next(user.data.user)
+    } else {
+      this.currentUser.next(false)
+    }
+  }
+
+  signUp(credentials: { email:string; password:string }) {
+    return this.supabase.auth.signUp(credentials)
+  }
+
+  signIn(credentials: { email:string; password:string }) {
+    return this.supabase.auth.signInWithPassword(credentials)
+  }
+
+  sendPwReset(email:string) {
+    return this.supabase.auth.resetPasswordForEmail(email)
+  }
+
+  async signOut() {
+    await this.supabase.auth.signOut()
+    this.router.navigateByUrl('/', { replaceUrl: true })
+  }
+
+  getCurrentUser(): Observable<User | boolean> {
+    return this.currentUser.asObservable()
+  }
+
+  getCurrentUserId(): string | null {
+    if (this.currentUser.value) {
+      return (this.currentUser.value as User).id
+    } else {
+      return null
+    }
+  }
+
+  signInWithEmail(email: string) {
+    return this.supabase.auth.signInWithOtp({ email })
+  }
+}
