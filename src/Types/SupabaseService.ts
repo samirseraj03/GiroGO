4affb11b504109077df3b0504b019721577c01a9
Isam <nfs.issamhajji@gmail.com>
2024-02-27 19:52:17 +0100

    Merging changes from local to dev branch
    Added Login and register pages.
    Added supabase authentication

--- src/Types/SupabaseService.ts
+++ src/Types/SupabaseService.ts
@@ -1,5 +1,4 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
-import { data } from 'azure-maps-control';
 
 
 // importamos los interfaces :
@@ -12,7 +11,6 @@ export interface Location {
   export  interface User {
     name: string;
     email: string;
-    password : string;
     user_type: UserType;
     location_id?: number;
     address?: string;
@@ -33,30 +31,9 @@ export interface Location {
   export interface carType {
     type: 'car' | 'motorcycle';
   }
-
-  export interface Travel {
-    created_at : Date
-    finalized_at : number
-    driver_id : number
-    user_id : number
-    Total : number
-    distance_km : number
-    initial_lat : number
-    initial_lon : number
-    finalized_lat : number
-    finalized_lon : number
-    initial_address : string
-    finalized_address : string
-  }
-
-  export interface Rating {
-    travel_id : number
-    driver_id : number
-    user_id : number
-    rating : number
-    comments : string
-  }
   
+
+
 
 export default class SupabaseService {
     public supabase: SupabaseClient;
@@ -76,36 +53,34 @@ export default class SupabaseService {
         this.supabase = createClient(supabaseUrl, supabaseKey);
     }
 
-    async  insertUser(newUser: User, location: Location): Promise<any> {
-      try {
-        // Insertamos la ubicación primero
-        const { data: insertedLocation, error: locationError } = await this.supabase
+    async  insertUser(newUser: User, location: Location): Promise<void> {
+        try {
+          // Insertamos la ubicación primero
+          const { data: insertedLocation, error: locationError } = await this.supabase
             .from('locations')
             .insert(location)
-            .select('*');
-    
-        if (locationError) {
+            .single() as { data: Location | null, error: any };
+      
+          if (locationError) {
             throw locationError;
-        } else {
-            // Acceder al ID del registro insertado en la ubicación
-            // Asignamos la ID de la ubicación al nuevo usuario
-              newUser.location_id = insertedLocation[0].id;
-            // Insertamos el nuevo usuario
-            const { data: insertedUser, error: userError } = await this.supabase
-                .from('users')
-                .insert(newUser)
-                .select();
-    
-            if (userError) {
-                throw userError;
-            }
-    
-            console.log('User inserted:', insertedUser);
-            return insertedUser[0].id
+          }
+          else {
+             newUser.location_id = insertedLocation!.id;
+          }     
+          // Insertamos el nuevo usuario
+          const { data: insertedUser, error: userError } = await this.supabase
+            .from('users')
+            .insert(newUser)
+            .single();
+      
+          if (userError) {
+            throw userError;
+          }
+      
+          console.log('User inserted:', insertedUser);
+        } catch (error) {
+          console.error('Error inserting user:', error);
         }
-    } catch (error) {
-        console.error('Error inserting user:', error);
-    }
       }
       
       // Función para insertar un nuevo vehículo en la base de datos
@@ -124,210 +99,8 @@ export default class SupabaseService {
         } catch (error) {
           console.error('Error inserting vehicle:', error);
         }
-
-      }
-
-      async InsertTravel( newTravel : Travel) : Promise<void>{
-        try {
-          // Insertamos el nuevo vehículo
-          const { data: insertedTravel, error: travelerror } = await this.supabase
-            .from('travels')
-            .insert(newTravel);
-      
-          if (travelerror) {
-            throw travelerror;
-          }
-      
-          console.log('travel inserted:', insertedTravel);
-        } catch (error) {
-          console.error('Error inserting travel:', error);
-        }
-      }
-
-      async InsertRating( newTravel : Travel) : Promise<void>{
-        try {
-          // Insertamos el nuevo vehículo
-          const { data: InsertedRating, error: Ratingerror } = await this.supabase
-            .from('travels')
-            .insert(newTravel);
-      
-          if (Ratingerror) {
-            throw Ratingerror;
-          }
-      
-          console.log('Inserted Rating:', InsertedRating);
-        } catch (error) {
-          console.error('Error inserting rating:', error);
-        }
-      }
-
-
-      async getUserWithEmail(user: User): Promise<void> {
-        try {
-            // Obtenemos el usuario según el objeto proporcionado
-            const { data: fetchedUser, error: userError } = await this.supabase
-                .from('users')
-                .select('*')
-                .eq('email', user.email) // Filtro por nombre, puedes cambiar el campo de filtro según tus necesidades
-                .single();
-    
-            if (userError) {
-                throw userError;
-            }
-    
-            console.log('User fetched:', fetchedUser);
-        } catch (error) {
-            console.error('Error fetching user:', error);
-        }
-    }
-
-    async getUser(id: any): Promise<any> {
-      try {
-          // Obtenemos el usuario según el objeto proporcionado
-          const { data: fetchedUser, error: userError } = await this.supabase
-              .from('users')
-              .select('*')
-              .eq('id', id) // Filtro por nombre, puedes cambiar el campo de filtro según tus necesidades
-              .single();
-  
-          if (userError) {
-              throw userError;
-          }
-          console.log('User fetched:', fetchedUser);
-          return fetchedUser
-      } catch (error) {
-          console.error('Error fetching user:', error);
-    }
-  }
-
-    async getTravel(id: any): Promise<void> {
-      try {
-          // Obtenemos el usuario según el objeto proporcionado
-          const { data: fetchedUser, error: userError } = await this.supabase
-              .from('travels')
-              .select('*')
-              .eq('id', id) // Filtro por nombre, puedes cambiar el campo de filtro según tus necesidades
-              .single();
-  
-          if (userError) {
-              throw userError;
-          }
-          console.log('travels fetched:', fetchedUser);
-      } catch (error) {
-          console.error('Error fetching user:', error);
-    }
-  }
-    // obtener travels del usuario para mostrarlos en la seccion de travels
-    async getTravels(user_id: any): Promise<void> {
-      try {
-          // Obtenemos el usuario según el objeto proporcionado
-          const { data: fetchedUser, error: userError } = await this.supabase
-              .from('travels')
-              .select('*')
-              .eq('user_id', user_id) // Filtro por nombre, puedes cambiar el campo de filtro según tus necesidades
-              .single();
-  
-          if (userError) {
-              throw userError;
-          }
-          console.log('travels fetched:', fetchedUser);
-      } catch (error) {
-          console.error('Error fetching user:', error);
-    }
 }
 
-
-async getRating(id: any): Promise<void> {
-  try {
-      // Obtenemos el usuario según el objeto proporcionado
-      const { data: fetchedUser, error: userError } = await this.supabase
-          .from('ratings')
-          .select('*')
-          .eq('id', id) // Filtro por nombre, puedes cambiar el campo de filtro según tus necesidades
-          .single();
-
-      if (userError) {
-          throw userError;
-      }
-      console.log('ratings fetched:', fetchedUser);
-  } catch (error) {
-      console.error('Error fetching user:', error);
 }
-}
-// obtener travels del usuario para mostrarlos en la seccion de travels
-async getRatings(user_id: any): Promise<void> {
-  try {
-      // Obtenemos el usuario según el objeto proporcionado
-      const { data: fetchedUser, error: userError } = await this.supabase
-          .from('ratings')
-          .select('*')
-          .eq('user_id', user_id) // Filtro por nombre, puedes cambiar el campo de filtro según tus necesidades
-          .single();
-
-      if (userError) {
-          throw userError;
-      }
-      console.log('ratings fetched:', fetchedUser);
-  } catch (error) {
-      console.error('Error fetching user:', error);
-}
-}
-  // juntamos las tablas de user y location para enviarlos a donde se deben desplegar
-  async getUsersWithLocations(): Promise<void> {
-    try {
-        // Consulta para obtener usuarios con ubicaciones
-        const { data: users, error: usersError } = await this.supabase
-            .from('users')
-            .select('*');
-
-        if (usersError) {
-            throw usersError;
-        }
-
-        // Consulta para obtener las ubicaciones de los usuarios
-        const { data: locations, error: locationsError } = await this.supabase
-            .from('locations')
-            .select('*');
-
-        if (locationsError) {
-            throw locationsError;
-        }
-
-        // Combinar los resultados de las dos consultas
-        const usersWithLocations = users.map(user => {
-            const location = locations.find(loc => loc.id === user.location_id);
-            return { ...user, location };
-        });
-
-        console.log('Users with locations:', usersWithLocations);
-    } catch (error) {
-        console.error('Error fetching users with locations:', error);
-    }
-}
-
-  async UpdateUser(userIdToUpdate: any , updatedUserData: any){
-    try {
-    
-      // Realizar la operación de actualización
-      const { data: updatedUser, error: updateError } = await this.supabase
-          .from('users')
-          .update(updatedUserData)
-          .match({ id: userIdToUpdate })
-          .single();
-      if (updateError) {
-          throw updateError;
-      }
-  
-      console.log('User updated:', updatedUser);
-  } catch (error) {
-      console.error('Error updating user:', error);
-  }
-}
-
-
-
-
-}
-
 
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AuthGuard } from './guards/auth.guard'
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
@@ -7,6 +8,14 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
   },
 ];
 @NgModule({
